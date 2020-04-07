1d05c070c4c14df8d35abe2cf2da851607667fea
Danilo Martinelli <danilomartinelli429@gmail.com>
2020-04-07 14:48:03 -0300

    change Design room name

--- backend/app/controllers/rooms.controller.js
+++ backend/app/controllers/rooms.controller.js
@@ -13,7 +13,7 @@ const fetchFromFile = () => {
   const roomsData = fs.readFileSync(roomFilePath);
   const roomsDetail = JSON.parse(roomsData);
 
-  return new Promise(resolve => resolve(roomsDetail));
+  return new Promise((resolve) => resolve(roomsDetail));
 };
 
 const createRoomFileSync = () => {
@@ -21,42 +21,42 @@ const createRoomFileSync = () => {
     {
       id: uuid(),
       name: "Geral",
-      externalMeetUrl: "https://meet.google.com/vrx-phky-zch"
+      externalMeetUrl: "https://meet.google.com/vrx-phky-zch",
     },
     {
       id: uuid(),
       name: "Dev",
-      externalMeetUrl: "https://meet.google.com/qpt-pmji-ums"
+      externalMeetUrl: "https://meet.google.com/qpt-pmji-ums",
     },
     {
       id: uuid(),
       name: "Adm",
-      externalMeetUrl: "https://meet.google.com/tie-yetg-ngf"
+      externalMeetUrl: "https://meet.google.com/tie-yetg-ngf",
     },
     {
       id: uuid(),
-      name: "Design (Somente Caio Permitido)",
-      externalMeetUrl: "https://meet.google.com/uyg-zyrd-rsw"
+      name: "Design",
+      externalMeetUrl: "https://meet.google.com/uyg-zyrd-rsw",
     },
     {
       id: uuid(),
       name: "Outros",
-      externalMeetUrl: "https://meet.google.com/csh-atho-mao"
-    }
+      externalMeetUrl: "https://meet.google.com/csh-atho-mao",
+    },
   ];
 
   fs.mkdirSync(path.dirname(roomFilePath), { recursive: true });
   fs.writeFileSync(roomFilePath, JSON.stringify(roomsData));
 };
 
-const fetchFromEnvironment = env => {
+const fetchFromEnvironment = (env) => {
   const roomsData = env.ROOMS_DATA;
   const roomsDetail = JSON.parse(roomsData);
 
-  return new Promise(resolve => resolve(roomsDetail));
+  return new Promise((resolve) => resolve(roomsDetail));
 };
 
-const fetchRooms = strategy => {
+const fetchRooms = (strategy) => {
   switch (strategy) {
     // TODO add suport to fetch from endpoint
     case "ENVIRONMENT":
